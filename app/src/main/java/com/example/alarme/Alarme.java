b46eec1c9abf638da895adca2b460f577497f534
Walisson Rodrigo <walisson.rodrigo@sagatechbrasil.com.br>
2019-11-20 22:42:13 -0300

    Botao de Desligar Alarme

--- app/src/main/java/com/example/alarme/Alarme.java
+++ app/src/main/java/com/example/alarme/Alarme.java
@@ -5,18 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.media.AudioAttributes;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.view.View;
+import android.widget.Button;
 
 public class Alarme extends AppCompatActivity {
 
-  
+    private Button btCancelar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarme);
+        btCancelar = findViewById(R.id.btCancelar);
 
-        Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        final Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
         long milliseconds = 10000;
-        vibrator.vibrate(milliseconds);
+        long[] machaImperial = new long[]{500,110,500,110,450,110,200,110,170,40,450,110,200,110,170,40,500};
+        //vibrator.vibrate(milliseconds);
+        vibrator.vibrate(machaImperial,-1);
+
+        btCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vibrator.cancel();
+            }
+        });
     }
 
 
