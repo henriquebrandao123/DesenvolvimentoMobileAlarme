a8015b23afd485203c25074c9a7d29f560a18789
Walisson Rodrigo <walisson.rodrigo@sagatechbrasil.com.br>
2019-11-20 21:57:14 -0300

    Vibrar o telefone quando o despertador e acionado

--- app/src/main/java/com/example/alarme/Alarme.java
+++ app/src/main/java/com/example/alarme/Alarme.java
@@ -2,13 +2,23 @@ package com.example.alarme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioAttributes;
 import android.os.Bundle;
+import android.os.Vibrator;
 
 public class Alarme extends AppCompatActivity {
 
+  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarme);
+
+        Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        long milliseconds = 10000;
+        vibrator.vibrate(milliseconds);
     }
+
+
+
 }
