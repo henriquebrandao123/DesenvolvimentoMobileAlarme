9161fbef55be06e2aa3a2e06b734a2088986f69b
Walisson Rodrigo <walisson.rodrigo@sagatechbrasil.com.br>
2019-11-20 21:05:18 -0300

    Adcionei a função de agendar alarme

--- app/src/main/java/com/example/alarme/MainActivity.java
+++ app/src/main/java/com/example/alarme/MainActivity.java
@@ -9,7 +9,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.webkit.ServiceWorkerController;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
@@ -18,56 +20,74 @@ import java.util.Calendar;
 public class MainActivity extends AppCompatActivity  {
 
     private Button btAlarme;
+    private TextView status;
+
+    private int currentHour;
+    private int currentMinute;
+    private int seconds;
+    private long time;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        status = findViewById(R.id.txtStatus);
         btAlarme = findViewById(R.id.btAlarme);
+
         btAlarme.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                Calendar c = Calendar.getInstance();
+
+                currentHour = c.get(Calendar.HOUR_OF_DAY);
+                currentMinute = c.get(Calendar.MINUTE);
 
                 TimePickerDialog timePickerDialog = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
 
+                        seconds = (hourOfDay * 3600 +  minute * 60) - (currentHour * 3600 +  currentMinute * 60) ;
+
+                        Calendar c = Calendar.getInstance();
+
+                        c.setTimeInMillis(System.currentTimeMillis());
+
+                        c.add(Calendar.SECOND, seconds);
+
+                        time = c.getTimeInMillis();
+
+                        status.setText(""+seconds);
+                        agendar(time);
+
+
+
+
                     }
-                }, 0, 0, false);
+                }, currentHour, currentMinute, true);
 
                 timePickerDialog.show();
-               
-                //agendar();
+
+
+
                 //finish();
             }
         });
     }
 
 
-    public void agendar()
+    public void agendar(long timer)
     {
         Intent it = new Intent("EXECUTAR_ALARME");
         PendingIntent p = PendingIntent.getBroadcast(MainActivity.this, 0, it, 0);
-        // precisamos pegar agora + 10segundos
-
-        Calendar c = Calendar.getInstance();
-
-        c.setTimeInMillis(System.currentTimeMillis());
-
-        c.add(Calendar.SECOND, 10); // +10 segundos
-
 
         // agendar o alarme
 
         AlarmManager alarme = (AlarmManager) getSystemService(ALARM_SERVICE);
 
-        long time = c.getTimeInMillis();
+        alarme.set(AlarmManager.RTC_WAKEUP, timer, p);
 
-        alarme.set(AlarmManager.RTC_WAKEUP, time, p);
-
-
-
-        // debug:
         Toast.makeText(getApplication(),"Alarme Agendado ", Toast.LENGTH_LONG).show();
         Log.i("Alarme", "Alarme agendado!");
     }
